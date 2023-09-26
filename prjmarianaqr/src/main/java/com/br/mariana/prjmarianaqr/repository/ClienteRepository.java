1cb938073eb25b1edb9f7a775feaea05d4f5c191
MarianaQuintinoRibeiro <133990329+MarianaQuintinoRibeiro@users.noreply.github.com>
2023-09-26 07:52:34 -0300

    Delete prjmarianaqr directory

--- prjmarianaqr/src/main/java/com/br/mariana/prjmarianaqr/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.mariana.prjmarianaqr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.mariana.prjmarianaqr.entities.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-}
