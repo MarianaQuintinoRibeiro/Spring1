56dada7d7288378e4c085a2a82dbff21713cdd42
MarianaQuintinoRibeiro <133990329+MarianaQuintinoRibeiro@users.noreply.github.com>
2023-09-26 07:35:03 -0300

    Add files via upload

---
+++ prjmarianaqr/src/test/java/com/br/mariana/prjmarianaqr/PrjmarianaqrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mariana.prjmarianaqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjmarianaqrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
